24c9e64141d411f70abb349f5ee6b25a7d1fa38d
Abhishek <abhishekb740@gmail.com>
2024-04-25 21:04:47 +0530

    Middleware function written

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const etherscanUrl = "https://api.etherscan.io/api";
 const apiKey = process.env.ETHERSCAN_API_KEY;
 const ethereumPriceUrl = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr";
 
+const validateAddress = (req, res, next) => {
+    const { address } = req.params;
+    if (!/^0x[a-fA-F0-9]{40}$/.test(address)) {
+        return res.status(400).json({ error: "Invalid Ethereum address" });
+    }
+    next();
+};
+
+const logErrors = (err, req, res, next) => {
+    console.error("Error:", err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+};
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
